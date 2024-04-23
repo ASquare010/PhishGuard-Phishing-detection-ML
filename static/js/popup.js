0f98b1b93749d09d5791fd51f3a6008038bf52f2
Awais Amin <81298743+ASquare010@users.noreply.github.com>
2024-04-24 03:05:18 +0500

    front-end changes
    -User interface of extension
    {
      -separate tabs for url and email.
      -Clear to understand and navigate
      -header links drop down added
      -Removed all not needed Code
      -Html and Css cluster cleaned
    }
    
    -Exception handling
    {
     -User can not enter non link content in input fields
     -Drop down will not how submit Button until user is on the right page
     -Auto selection of all urls to Pick and check form Header
    }
    -Api (back-end)
    {
     -Api key added and improved for the users and Admin
     -Admin Phase assed to retrain the model
     -and more
    }

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,6 +1,6 @@
 // popup.js
 const base = 'https://phishing-detection-357f9186c0b4.herokuapp.com/';
-// const base = 'http://172.20.193.196:5000';
+// const base = 'http://192.168.0.102:5000';
 
 const apiKey = 'aB3x8Yp2qR5sW9tZ';
 
@@ -8,20 +8,14 @@ const url = base+'/urlpredictExt';
 const email = base+'/emailpredictExt';
 
 
+// InitState Tab
+document.getElementById("urlsTab").addEventListener("click", function() {
+    openTab(0);
+  });
 
-document.getElementById('customBtn').addEventListener('click', function() {
-    var customDetectionDiv = document.getElementById('customDetection');
-    if (customDetectionDiv) {
-        if (customDetectionDiv.hasAttribute('hidden')) {
-            customDetectionDiv.removeAttribute('hidden'); 
-            this.textContent = 'Hide custom options'; 
-        } else {
-            customDetectionDiv.setAttribute('hidden', true);
-            this.textContent = 'Show custom options'; 
-        }
-    }
-});
-
+document.getElementById("emailTab").addEventListener("click", function() {
+    openTab(1);
+  });
 
 
 function fetchData(url, dataToSend) {
@@ -44,45 +38,72 @@ function fetchData(url, dataToSend) {
     });
 }
 
+function onUrlBtnClick() {
+    const link = urlInput.value.trim(); // Get the URL value and trim whitespace
+    
+    var pattern = /^(http|https):\/\/[^ "]+$/;
 
-function getUrlResult(urlData)
-{
+    if(pattern.test(link)) {
+
+        let urlData = { 'url':link, 'key':apiKey };
+        document.getElementById('currentTabUrl').textContent = 'Checking Url';
+        getUrlResult(urlData);
+    } 
+    else{
+        alert('Invalid URL entered. Please enter a valid URL.');
+    }
+    
+}
+
+
+function onEmailBtnClick(){
+
+    var selectElement = document.getElementById("selectOption");
+    var selectedIndex = selectElement.selectedIndex;
+    var selectedOption = selectElement.options[selectedIndex];
+    var selectedValue = selectedOption.value;
+    let urlData = { 'url':selectedValue, 'key':apiKey };
+    
+    document.getElementById('currentEmail').textContent = 'Checking Url';
+
+    getUrlResult(urlData,'currentEmail');    
+}
+
+
+function getUrlResult(urlData,currentResult="currentTabUrl") {
     fetchData(url, urlData)
     .then(data => {
         // Handle the data returned from the server
-        var currentTabUrl = document.getElementById('currentTabUrl');
+        var currentTabUrl = document.getElementById(currentResult);
         currentTabUrl.textContent = data['result'];
         
 
         if (data['result'].includes('Safe')) {
-            currentTabUrl.style.backgroundColor = "#91EF91";
+            currentTabUrl.style.backgroundColor = "#006666";
             
         } else {
-            currentTabUrl.style.backgroundColor = "red";
-            currentTabUrl.style.color = "white";
-        }
+            currentTabUrl.style.backgroundColor = "#993333";
+         }
     })
     .catch(error => {
         // Handle error
-        document.getElementById('currentTabUrl').textContent = 'Error: ' + error.message;
+        document.getElementById(currentResult).textContent = 'Error: ' + error.message;
         
     });
 }
 
 
-function getEmailResult(emailData)
-{
+function getEmailResult(emailData) {
     fetchData(email, emailData)
     .then(data => {
         var currentEmail =document.getElementById('currentEmail')
         currentEmail.textContent = data['result'];
         
         if (data['result'].includes('Safe')) {
-            currentEmail.style.backgroundColor = "#91EF91";
+            currentEmail.style.backgroundColor = "#006666";
         } else {
-            currentEmail.style.backgroundColor = "red";
-            currentEmail.style.color = "white";
-        }
+            currentEmail.style.backgroundColor = "#993333";
+         }
     })
     .catch(error => {
         // Handle error
@@ -90,67 +111,51 @@ function getEmailResult(emailData)
     });
 }
 
-function urlCheck(tabs)
-{
+function urlCheck(tabs) {
     
     var currentTab = tabs[0];
     var currentTabUrl = currentTab.url;
-    submitButtonUrl = document.getElementById('submitUrl');
-    submitButtonEmail = document.getElementById('submitEmail');
+    var submitButtonUrl = document.getElementById('submitUrl');
     let urlData = { 'url':currentTabUrl, 'key':apiKey};
 
     document.getElementById('currentPageink').textContent = currentTabUrl.substring(0, 25)+"...";
-
     getUrlResult(urlData)
 
 
-    submitButtonUrl.addEventListener('click', function() {
-        const link = urlInput.value.trim(); // Get the URL value and trim whitespace
-        
-        var pattern = /^(http|https):\/\/[^ "]+$/;
-
-        if(pattern.test(link)) {
-
-            let urlData = { 'url':link, 'key':apiKey };
-            document.getElementById('currentTabUrl').textContent = 'Checking Url';
-            getUrlResult(urlData);
-        } 
-        else{
-            alert('Invalid URL entered. Please enter a valid URL.');
-        }
-        
-    });
-
-    submitButtonEmail.addEventListener('click', function() {
-
-        var mail =  document.getElementById('emailInput').value;
-        
-        let emailData = { 'email':mail , 'key':apiKey};
-        
-        document.getElementById('currentEmail').textContent = 'Checking Email';
-    
-        getEmailResult(emailData);
-        
-    });
+    submitButtonUrl.addEventListener('click', onUrlBtnClick);
 }
 
 
 function getEmailHeader(results) {
         
     var message = document.getElementById('emailHeader');
+    var submitButtonEmail = document.getElementById('submitEmail');
     var bodyHTML =results[0].result
     var parser = new DOMParser();
     var bodyDOM = parser.parseFromString(bodyHTML, 'text/html');
     var emailHeader = bodyDOM.querySelector('#raw_message_text');
-    
-    document.getElementById('emailInput').value = emailHeader.innerText;
-
-    
+    var selectOption = document.getElementById("selectOption")
     var match = emailHeader.innerText.match(/From:.*?(?=>)/);
-    message.innerText =match+">";
+    var urlRegex = /(https?:\/\/[^\s]+)/gi;
+    var matches = emailHeader.innerText.match(urlRegex);
+    var submitEmail = document.getElementById('submitEmail');
     let emailData = { 'email':emailHeader.innerText , 'key':apiKey};
+
+    submitEmail.hidden = false;
+    selectOption.innerHTML="";
+    message.innerText =match+">";
     getEmailResult(emailData);
 
+    if (matches) {
+        matches.forEach(function(url) {
+            var option = document.createElement("option");
+            option.innerText = url.substring(0, 20)+"..";
+            option.value = url;
+            selectOption.appendChild(option);
+        });
+    }
+
+    submitButtonEmail.addEventListener('click', onEmailBtnClick);
 }
 
 
@@ -165,8 +170,7 @@ function DOMtoString(selector) {
 }
 
 
-function emailCheck(tabs)
-{
+function emailCheck(tabs) {
     var message = document.getElementById('currentEmail');
     var activeTab = tabs[0];
     var activeTabId = activeTab.id;
@@ -180,20 +184,42 @@ function emailCheck(tabs)
     });
 }
 
-function main() {
+
+function openTab(evt) {
+    // Hide all tab contents
     
+    var urlBtn = document.getElementById("urlsTab");
+    var emailBtn = document.getElementById("emailTab");
+    var emailTab = document.getElementById("Email");
+    var urlTab = document.getElementById("URLs");
 
-    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
- 
-        urlCheck(tabs)
-        emailCheck(tabs)
+    if(evt === 0) {
+        emailBtn.classList.remove("active");
+        urlBtn.classList.add("active");
+        emailTab.hidden = true;
+        urlTab.hidden = false;
+        chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+            urlCheck(tabs)
+        })
+    }
+    else if(evt === 1) {
+        urlBtn.classList.remove("active");
+        emailBtn.classList.add("active");
+        urlTab.hidden = true;
+        emailTab.hidden = false;
+        chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+            emailCheck(tabs)    
+        })
+    }
+    
+}
+  
 
-    })
+function main() {
+    openTab(0)
 }
 
 
 
 // void Main()
 window.onload = main;
-
-
